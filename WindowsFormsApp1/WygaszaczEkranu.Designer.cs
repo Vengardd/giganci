702b6782e05ce5705f490a45f66c35348d52c7b6
Vengardd <vengardd@users.noreply.github.com>
2020-03-31 18:12:46 +0200

    init commit

---
+++ WindowsFormsApp1/WygaszaczEkranu.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class WygaszaczEkranu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // WygaszaczEkranu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "WygaszaczEkranu";
+            this.Text = "Wygaszacz ekranu";
+            this.Load += new System.EventHandler(this.WygaszaczEkranu_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyPressed);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MouseClicked);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMoved);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ WindowsFormsApp1/WygaszaczEkranu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class WygaszaczEkranu : Form
+    {
+        private int x, y; // zmienne na współrzedne kursora myszki
+        private string[] obrazki; // tablica na obrazki
+        private int iloscObrazkow; // ilość załadowanych obrazków
+        private int numerObrazka; // numer aktualnie wyświetlanego obrazka
+
+        public WygaszaczEkranu()
+        {
+            InitializeComponent();
+            BackgroundImageLayout = ImageLayout.Stretch;
+            DoubleBuffered = true;
+            WindowState = FormWindowState.Minimized;
+            FormBorderStyle = FormBorderStyle.None;
+            iloscObrazkow = 0;
+            numerObrazka = 0;
+        }
+
+        private void WygaszaczEkranu_Load(object sender, EventArgs e)
+        {
+            x = Cursor.Position.X;
+            y = Cursor.Position.Y;
+            string folder = @"C:\Users\Vengard\source\repos\WindowsFormsApp1\WindowsFormsApp1\obrazki\";
+            try
+            {
+                obrazki = Directory.GetFiles(folder);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Błąd w trakcie pobierania obrazków");
+                Application.Exit();
+            }
+
+            iloscObrazkow = obrazki.Length;
+
+            if (iloscObrazkow == 0)
+            {
+                MessageBox.Show("Brak obrazków dla wygaszacza");
+                Application.Exit();
+            }
+
+            BackgroundImage = Image.FromFile(obrazki[numerObrazka]);
+            Cursor.Hide();
+            WindowState = FormWindowState.Maximized;
+            timer.Start();
+        }
+
+        private void MouseMoved(object sender, MouseEventArgs e)
+        {
+            if (e.X != x || e.Y != y)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void MouseClicked(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void KeyPressed(object sender, KeyEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                BackgroundImage = Image.FromFile(obrazki[numerObrazka++]);
+                if (numerObrazka >= iloscObrazkow)
+                {
+                    numerObrazka = 0;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Błąd podczas pobierania pliku");
+            }
+        }
+    }
+}
+
